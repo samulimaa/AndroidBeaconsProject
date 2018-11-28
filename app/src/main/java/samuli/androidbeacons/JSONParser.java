ca9f702d4b9c11d46f79bff868453cea508c21e0
Samuli-M <samulimaatt@gmail.com>
2018-11-28 11:50:31 +0200

    More parsing

--- app/src/main/java/samuli/androidbeacons/JSONParser.java
+++ app/src/main/java/samuli/androidbeacons/JSONParser.java
@@ -4,6 +4,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class JSONParser {
@@ -51,8 +54,67 @@ public class JSONParser {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
         return beaconTimeMap;
+    }
+
+    static HashMap<String, Integer> parseBeaconDates(JSONObject jsonObject, String beaconName) {
+        HashMap hashMap = new HashMap<String, Integer>();
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for(int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectEntry = jsonArray.getJSONObject(i);
+                if (jsonObjectEntry.getString("beacon_name").equals(beaconName)) {
+                    String date = jsonObjectEntry.getString("date");
+                    int time = jsonObjectEntry.getInt("seconds");
+                    if (hashMap.containsKey(date)) {
+                        int oldTime = (int) hashMap.get(date);
+                        hashMap.remove(date);
+                        hashMap.put(date, oldTime + time);
+                    } else {
+                        hashMap.put(date, time);
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return hashMap;
+    }
+
+    static int parseBeaconsAmount(JSONObject jsonObject) {
+        ArrayList<String> beaconNames = new ArrayList<>();
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectEntry = jsonArray.getJSONObject(i);
+                String beaconName = jsonObjectEntry.getString("beacon_name");
+                if (!beaconNames.contains(beaconName)) {
+                    beaconNames.add(beaconName);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return beaconNames.size();
+    }
+
+    static ArrayList<String> parseAndSortBeaconNames(JSONObject jsonObject) {
+        ArrayList<String> beaconNames = new ArrayList<>();
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectEntry = jsonArray.getJSONObject(i);
+                String beaconName = jsonObjectEntry.getString("beacon_name");
+                if (!beaconNames.contains(beaconName)) {
+                    beaconNames.add(beaconName);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Collections.sort(beaconNames);
+        return beaconNames;
     }
 
 }
